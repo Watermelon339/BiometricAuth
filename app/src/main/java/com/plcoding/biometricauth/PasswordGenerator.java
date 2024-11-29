573abbe448411ef845bf177bc503344b545c847a
Justinohayo <159392063+Justinohayo@users.noreply.github.com>
2024-11-29 04:55:01 -0800

    Update PasswordGenerator.java

--- app/src/main/java/com/plcoding/biometricauth/PasswordGenerator.java
+++ app/src/main/java/com/plcoding/biometricauth/PasswordGenerator.java
@@ -2,8 +2,11 @@ package com.plcoding.biometricauth;
 
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.ContentProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -12,18 +15,32 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.plcoding.biometricauth.models.LowerCaseGenerator;
-import com.plcoding.biometricauth.models.NumericGenerator;
-import com.plcoding.biometricauth.models.UpperCaseGenerator;
-import com.plcoding.biometricauth.models.SpecialCharGenerator;
+import com.plcoding.biometricauth.models.database.DatabaseHelper;
+import com.plcoding.biometricauth.models.database.SavePassword;
+import com.plcoding.biometricauth.models.generators.LowerCaseGenerator;
+import com.plcoding.biometricauth.models.generators.NumericGenerator;
+import com.plcoding.biometricauth.models.generators.UpperCaseGenerator;
+import com.plcoding.biometricauth.models.generators.SpecialCharGenerator;
+import com.plcoding.biometricauth.models.password.Password;
+import com.plcoding.biometricauth.models.password.PasswordAdapter;
+import com.plcoding.biometricauth.models.utils.EncryptionUtils;
+
+import java.util.List;
+
+import javax.crypto.SecretKey;
 
 public class PasswordGenerator extends AppCompatActivity {
 
     private EditText editPasswordSize;
     private TextView textPasswordGenerated,textErrorMessage;
     private CheckBox checkLower, checkUpper,checkSpecialChar, checkNumeric;
-    private Button btnGenerate, btnCopy;
+    private Button btnGenerate, btnCopy, btnSave;
+
+    private RecyclerView reclyclerSavedPasswords;
+    private PasswordAdapter passwordAdapter;
 
 
     @Override
@@ -34,6 +51,32 @@ public class PasswordGenerator extends AppCompatActivity {
 
         initViews();
         clickListeners();
+        displaySavedPasswords(); //TODO: TO BE REMOVED LATER
+    }
+
+    private void displaySavedPasswords() {
+        try {
+            // Retrieve or generate the SecretKey
+            SecretKey secretKey = retrieveOrGenerateKey();
+
+            DatabaseHelper db = new DatabaseHelper(PasswordGenerator.this);
+            List<Password> passwordList = db.getPasswordList(secretKey);
+            Log.e("PWD_LIST", passwordList.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("PWD_LIST", "Error retrieving saved passwords: " + e.getMessage());
+        }
+    }
+
+    private SecretKey retrieveOrGenerateKey() {
+        try {
+            // Generate or retrieve the key
+            return EncryptionUtils.generateKey(); // Use your secure key management logic
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("KEY_ERROR", "Failed to generate or retrieve encryption key: " + e.getMessage());
+            throw new RuntimeException("Failed to generate or retrieve encryption key");
+        }
     }
 
     private void clickListeners() {
@@ -47,20 +90,22 @@ public class PasswordGenerator extends AppCompatActivity {
                 return;
             }
 
-            com.plcoding.biometricauth.models.PasswordGenerator.clear();
-            if(checkLower.isChecked()) com.plcoding.biometricauth.models.PasswordGenerator.add(new LowerCaseGenerator());
-            if(checkNumeric.isChecked()) com.plcoding.biometricauth.models.PasswordGenerator.add(new NumericGenerator());
-            if(checkUpper.isChecked()) com.plcoding.biometricauth.models.PasswordGenerator.add(new UpperCaseGenerator());
-            if(checkSpecialChar.isChecked()) com.plcoding.biometricauth.models.PasswordGenerator.add(new SpecialCharGenerator());
+            com.plcoding.biometricauth.models.generators.PasswordGenerator.clear();
+            if(checkLower.isChecked()) com.plcoding.biometricauth.models.generators.PasswordGenerator.add(new LowerCaseGenerator());
+            if(checkNumeric.isChecked()) com.plcoding.biometricauth.models.generators.PasswordGenerator.add(new NumericGenerator());
+            if(checkUpper.isChecked()) com.plcoding.biometricauth.models.generators.PasswordGenerator.add(new UpperCaseGenerator());
+            if(checkSpecialChar.isChecked()) com.plcoding.biometricauth.models.generators.PasswordGenerator.add(new SpecialCharGenerator());
 
 
-            if(com.plcoding.biometricauth.models.PasswordGenerator.isEmpty()){
+            if(com.plcoding.biometricauth.models.generators.PasswordGenerator.isEmpty()){
                 textErrorMessage.setText("Please select at least one password content type");
                 return;
             }
 
-            String password = com.plcoding.biometricauth.models.PasswordGenerator.generatePassword(passwordSize);
+            String password = com.plcoding.biometricauth.models.generators.PasswordGenerator.generatePassword(passwordSize);
             textPasswordGenerated.setText(password);
+
+            btnSave.setEnabled(true);
 
         });
 
@@ -68,6 +113,13 @@ public class PasswordGenerator extends AppCompatActivity {
             ClipboardManager manager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
             manager.setPrimaryClip(ClipData.newPlainText("password",textPasswordGenerated.getText().toString()));
             Toast.makeText(this, "Password Copied", Toast.LENGTH_SHORT).show();
+        });
+
+        btnSave.setOnClickListener(view ->{
+            String genPwd = textPasswordGenerated.getText().toString();
+            Intent intent = new Intent(PasswordGenerator.this, SavePassword.class);
+            intent.putExtra("pwd",genPwd);
+            startActivity(intent);
         });
     }
 
@@ -81,5 +133,11 @@ public class PasswordGenerator extends AppCompatActivity {
         checkNumeric = findViewById(R.id.check_numeric);
         btnGenerate = findViewById(R.id.btn_generate);
         btnCopy = findViewById(R.id.btn_copy);
+        btnSave = findViewById(R.id.btn_save);
+
+        btnSave.setEnabled(false);
+
+        reclyclerSavedPasswords = findViewById(R.id.recycler_saved_passwords);
+        reclyclerSavedPasswords.setLayoutManager(new LinearLayoutManager(this));
     }
 }
